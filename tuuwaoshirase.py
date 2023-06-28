a877da42a653c9c55794a5c5303669e10ae35087
Ta-585 <132693608+Ta-585@users.noreply.github.com>
2023-06-28 23:28:13 +0900

    Update tuuwaoshirase.py

--- tuuwaoshirase.py
+++ tuuwaoshirase.py
@@ -31,14 +31,14 @@ async def on_voice_state_update(member: discord.Member, before:discord.VoiceStat
             embed.add_field(name="始めた人", value=member.display_name, inline=False)
             embed.add_field(name="始めた時刻", value=datetime.datetime.now(pytz.timezone('Asia/Tokyo')), inline=False)
             embed.set_thumbnail(url=member.display_avatar.url)
-            e_time[member.id]=datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
+            e_time[after.channel.id]=datetime.datetime.now(pytz.timezone('Asia/Tokyo'))
             await channel.send(content="@everyone", embed=embed)
     
     if before.channel and not after.channel and len(before.channel.members)==0:
         channel = bot.get_channel(channel_id)
         embed=discord.Embed(title="通話終了", color=0x6a5acd)
         embed.add_field(name="チャンネル", value=before.channel.name, inline=False)
-        embed.add_field(name="通話時間", value=datetime.datetime.now(pytz.timezone('Asia/Tokyo'))-e_time[member.id], inline=False)
+        embed.add_field(name="通話時間", value=datetime.datetime.now(pytz.timezone('Asia/Tokyo'))-e_time[before.channel.id], inline=False)
         await channel.send(embed=embed)
 
 async def main():
